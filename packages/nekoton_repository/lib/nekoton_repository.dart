d54e3c12fc5572645f94018c3429c38f3a2358f2
nesquikm <drapolyuk@gmail.com>
2023-01-31 12:58:39 +0400

    fix: analysis warnings

--- packages/nekoton_repository/lib/nekoton_repository.dart
+++ packages/nekoton_repository/lib/nekoton_repository.dart
@@ -5,6 +5,11 @@ import 'package:injectable/injectable.dart';
 export 'src/models/models.dart';
 export 'src/nekoton_repository.dart';
 
+// ignore_for_file: always_declare_return_types
+// ignore_for_file: inference_failure_on_function_return_type
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_annotate_public_apis
+
 // @microPackageInit => short const
 @InjectableInit.microPackage()
 initMicroPackage() {} // will not be called but needed for code generation
