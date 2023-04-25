36b868b45da325bcec028a84b4d3458db3655396
nesquikm <drapolyuk@gmail.com>
2023-04-25 10:54:11 +0400

    fix: add custom clean script

---
+++ scripts/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove build files generated code
+find . -type f -name "*_generated.*" -delete
+find . -type f -name "*.freezed.dart" -delete
+find . -type f -name "*.g.dart" -delete
+find . -type f -name "*.reflectable.dart" -delete
+
+# Remove DI config
+rm -rf lib/di/di.config.dart
