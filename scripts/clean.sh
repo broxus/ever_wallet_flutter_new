1e5372edd225f61ebbce289ac1f01e32c812818d
nesquikm <drapolyuk@gmail.com>
2023-04-27 22:45:36 +0400

    feat: add secrets (#31)

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -9,3 +9,11 @@ find . -type f -name "*.module.dart" -delete
 
 # Remove DI config
 rm -rf lib/di/di.config.dart
+
+# Sectets
+rm android/crystal.keystore
+rm android/key.properties
+rm android/fastlane/GooglePlayServiceAccount.json
+rm ios/fastlane/AuthKey_L4N29B6Z42.p8
+rm fastlane/FirebaseAPIKey.json
+rm fastlane/FirebaseADKey.json
