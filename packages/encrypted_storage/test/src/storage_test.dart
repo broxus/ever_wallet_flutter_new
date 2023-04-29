4423ccb44cbab9aa535d2588b8c225199f8be1bc
nesquikm <drapolyuk@gmail.com>
2023-04-29 15:15:13 +0400

    fix: some warnings

--- packages/encrypted_storage/test/src/storage_test.dart
+++ packages/encrypted_storage/test/src/storage_test.dart
@@ -2,7 +2,6 @@
 
 import 'package:encrypted_storage/src/storage.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 const String testDomainName0 = 'test domain name 0';
