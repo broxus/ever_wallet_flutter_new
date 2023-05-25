4c3321adc013d323ba0d954e09d186c311734a2b
nesquikm <drapolyuk@gmail.com>
2023-05-25 17:56:25 +0400

    fix: changelog generator for releases

--- scripts/get-changelog.sh
+++ scripts/get-changelog.sh
@@ -4,10 +4,9 @@ set -o pipefail
 
 echo "🌳 Getting changelog..."
 export BRANCH=`git branch | sed -n '/\* /s///p'`
-log=`git log -n 10 --decorate=auto`
-log_short=`git log -n 10 --decorate=auto | grep -v "commit" | grep -v "Author:" | grep -v "Author:" | grep -v "Date:"`
+log=`git log -n 20 --oneline --decorate=no | sed 's/[^a-zA-Z0-9():-\/\.#&_\ ]//g'`
 export CHANGELOG_STRING="Branch: $BRANCH "$'\n\n'"$log"
-export CHANGELOG_STRING_SHORT=$log_short
+export CHANGELOG_STRING_SHORT=$log
 
 echo "🛒🤖 Creating changelog for GP"
 mkdir -p android/fastlane/metadata/android/en-US/changelogs
