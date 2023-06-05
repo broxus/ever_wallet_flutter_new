abd35c07235761f51c27e59b191220f886f769bc
AlexA4 <alex.a4.25@yandex.ru>
2023-06-05 15:49:32 +0300

    fix: file format

--- packages/ui_components_lib/lib/components/input/input.dart
+++ packages/ui_components_lib/lib/components/input/input.dart
@@ -1,4 +1,4 @@
-//GENERATED BARREL FILE 
-export 'common_input.dart'; 
-export 'common_switch_input.dart'; 
-export 'selection_status_input.dart'; 
+//GENERATED BARREL FILE
+export 'common_input.dart';
+export 'common_switch_input.dart';
+export 'selection_status_input.dart';
