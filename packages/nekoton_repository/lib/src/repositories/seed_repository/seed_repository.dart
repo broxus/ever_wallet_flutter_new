526567f13e5efa7f186e82334dc238a4f23ae81b
Egor Komarov <egor.komarov@bf.rocks>
2023-06-26 16:32:01 +0200

    refactor: replace packages

--- packages/nekoton_repository/lib/src/repositories/seed_repository/seed_repository.dart
+++
@@ -1,3 +0,0 @@
-//GENERATED BARREL FILE
-export 'seed_repository_abstract.dart';
-export 'seed_repository_impl.dart';
--- packages/nekoton_repository/lib/src/repositories/transport_repository/transport_repository.dart
+++
@@ -1,3 +0,0 @@
-//GENERATED BARREL FILE
-export 'transport_repository_abstract.dart';
-export 'transport_repository_impl.dart';
