f0c3875cf06389cc07b02721e7fb44dcf2336246
nesquikm <drapolyuk@gmail.com>
2023-07-06 12:24:07 +0400

    feat: contact support (#143)
    * feat: add user initiated contact support feature
    
    * feat: report errors from PlatformDispatcher
    
    * feat: add automatic crash detection and reporting
    
    * fix: email body
    
    * fix: analyzer warnings
    
    * fix: a few remarks

---
+++ lib/feature/contact_support/widgets/contact_support/contact_support.dart
@@ -0,0 +1,3 @@
+export 'contact_support_email_send.dart';
+export 'contact_support_mode.dart';
+export 'contact_support_sheet.dart';
---
+++ lib/feature/contact_support/widgets/contact_support/contact_support_sheet.dart
@@ -0,0 +1,87 @@
+import 'package:app/feature/contact_support/contact_support.dart';
+import 'package:app/generated/generated.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ui_components_lib/ui_components_lib.dart';
+
+/// Helper function to show [ContactSupportSheet].
+Future<void> showContactSupportSheet({
+  required BuildContext context,
+  required ContactSupportMode mode,
+}) {
+  final title = switch (mode) {
+    ContactSupportMode.initiatedByUser =>
+      LocaleKeys.contactSupportBottomSheetHeaderInitiatedByUser.tr(),
+    ContactSupportMode.initiatedByCrash =>
+      LocaleKeys.contactSupportBottomSheetHeaderInitiatedByCrash.tr(),
+  };
+
+  final subtitle = switch (mode) {
+    ContactSupportMode.initiatedByUser =>
+      LocaleKeys.contactSupportBottomSheetTextInitiatedByUser.tr(),
+    ContactSupportMode.initiatedByCrash =>
+      LocaleKeys.contactSupportBottomSheetTextInitiatedByCrash.tr(),
+  };
+
+  return showCommonBottomSheet(
+    context: context,
+    title: title,
+    subtitle: subtitle,
+    body: (_, __) => BlocProvider(
+      create: (context) => ContactSupportBloc(),
+      child: BlocListener<ContactSupportBloc, ContactSupportState>(
+        listener: (context, state) {
+          state.whenOrNull(
+            initial: () {
+              Navigator.of(context).pop();
+            },
+          );
+        },
+        child: ContactSupportSheet(mode: mode),
+      ),
+    ),
+  );
+}
+
+class ContactSupportSheet extends StatelessWidget {
+  const ContactSupportSheet({
+    required this.mode,
+    super.key,
+  });
+
+  final ContactSupportMode mode;
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonText = switch (mode) {
+      ContactSupportMode.initiatedByUser =>
+        LocaleKeys.contactSupportBottomSheetButtonInitiatedByUser.tr(),
+      ContactSupportMode.initiatedByCrash =>
+        LocaleKeys.contactSupportBottomSheetButtonInitiatedByCrash.tr(),
+    };
+
+    return BlocBuilder<ContactSupportBloc, ContactSupportState>(
+      builder: (context, state) {
+        return ShapedContainerColumn(
+          mainAxisSize: MainAxisSize.min,
+          separator: const CommonDivider(),
+          margin: EdgeInsets.zero,
+          children: [
+            CommonButton.primary(
+              fillWidth: true,
+              text: buttonText,
+              isLoading: state.when(initial: () => false, busy: () => true),
+              onPressed: () => {
+                context.read<ContactSupportBloc>().add(
+                      ContactSupportEvent.sendEmail(
+                        mode,
+                      ),
+                    ),
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
