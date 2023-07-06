6cca3eccb9f28d48adc9f67e4a941fed9b068346
AlexA4 <alex.a4.25@yandex.ru>
2023-07-06 20:32:53 +0300

    feat: add wallet start screen

--- lib/feature/profile/key_detail/widgets/accounts_settings_sheet.dart
+++ lib/feature/profile/key_detail/widgets/accounts_settings_sheet.dart
@@ -1,15 +1,21 @@
+import 'package:app/app/service/service.dart';
 import 'package:app/di/di.dart';
 import 'package:app/feature/profile/profile.dart';
 import 'package:app/generated/generated.dart';
 import 'package:flutter/material.dart';
-import 'package:nekoton_repository/nekoton_repository.dart';
+import 'package:flutter/services.dart';
+import 'package:nekoton_repository/nekoton_repository.dart' hide Message;
 import 'package:ui_components_lib/ui_components_lib.dart';
 
 /// Helper function to show [AccountSettingsSheet].
+/// [showHiding] flag if hide/show item should be shown.
+/// [seeInExplorer] flag to open browser and explore account.
 Future<void> showAccountSettingsSheet({
   required BuildContext context,
   required Address address,
   bool showHiding = true,
+  bool seeInExplorer = false,
+  bool showCopyAddress = false,
 }) {
   return showCommonBottomSheet(
     context: context,
@@ -18,6 +24,8 @@ Future<void> showAccountSettingsSheet({
     body: (_, __) => AccountSettingsSheet(
       address: address,
       showHiding: showHiding,
+      seeInExplorer: seeInExplorer,
+      showCopyAddress: showCopyAddress,
     ),
   );
 }
@@ -27,6 +35,8 @@ class AccountSettingsSheet extends StatelessWidget {
   const AccountSettingsSheet({
     required this.address,
     required this.showHiding,
+    required this.seeInExplorer,
+    required this.showCopyAddress,
     super.key,
   });
 
@@ -35,6 +45,12 @@ class AccountSettingsSheet extends StatelessWidget {
 
   /// Flag if hide/show item should be shown.
   final bool showHiding;
+
+  /// Flag to open browser and explore account.
+  final bool seeInExplorer;
+
+  /// Flag to copy address to clipboard
+  final bool showCopyAddress;
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +98,37 @@ class AccountSettingsSheet extends StatelessWidget {
                   svg: Assets.images.edit.path,
                 ),
               ),
+              if (showCopyAddress)
+                CommonListTile(
+                  titleText: LocaleKeys.copyAddress.tr(),
+                  onPressed: () {
+                    Clipboard.setData(
+                      ClipboardData(text: account.address.address),
+                    );
+                    inject<MessengerService>().show(
+                      Message.successful(
+                        message: LocaleKeys.valueCopiedExclamation.tr(
+                          args: [account.address.toEllipseString()],
+                        ),
+                      ),
+                    );
+                  },
+                  contentColor: colors.textPrimary,
+                  trailing: CommonButtonIconWidget.svg(
+                    svg: Assets.images.copy.path,
+                  ),
+                ),
+              if (seeInExplorer)
+                CommonListTile(
+                  titleText: LocaleKeys.seeInExplorer.tr(),
+                  // TODO(alex-a4): add logic going to browser
+                  // ignore: no-empty-block
+                  onPressed: () {},
+                  contentColor: colors.textPrimary,
+                  trailing: CommonButtonIconWidget.svg(
+                    svg: Assets.images.planetInner.path,
+                  ),
+                ),
               CommonListTile(
                 titleText: LocaleKeys.deleteWord.tr(),
                 contentColor: colors.alert,
---
+++ lib/feature/wallet/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'account_card.dart';
+export 'add_new_account_card.dart';
+export 'network_selection_card.dart';
+export 'wallet_account_actions.dart';
+export 'wallet_accounts_body.dart';
+export 'wallet_bottom_panel.dart';
