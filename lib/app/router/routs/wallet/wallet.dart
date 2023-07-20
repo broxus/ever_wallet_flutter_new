747b0cc5b6d6d86dad747680d8683fc8bcf41af3
nesquikm <drapolyuk@gmail.com>
2023-07-20 13:31:41 +0400

    refactor: use stateful shell routes (#165)

--- lib/app/router/routs/wallet/wallet.dart
+++ lib/app/router/routs/wallet/wallet.dart
@@ -1,25 +1,22 @@
-import 'package:app/app/router/page_transitions.dart';
 import 'package:app/app/router/router.dart';
 import 'package:app/feature/add_seed/add_seed_enable_biometry/add_seed_enable_biometry.dart';
 import 'package:app/feature/wallet/wallet.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-/// Route that is root for wallet.
-GoRoute get walletRoute {
-  return GoRoute(
-    name: AppRoute.wallet.name,
-    path: AppRoute.wallet.path,
-    pageBuilder: (BuildContext context, GoRouterState state) =>
-        rootTabsTransitionPageBuilder(
-      context,
-      state,
-      const WalletPage(),
-    ),
+/// Branch that is root for wallet.
+StatefulShellBranch get walletBranch {
+  return StatefulShellBranch(
     routes: [
       GoRoute(
-        path: AppRoute.enableBiometryAfterOnboarding.path,
-        builder: (_, __) => const AddSeedEnableBiometryPage(),
+        name: AppRoute.wallet.name,
+        path: AppRoute.wallet.path,
+        builder: (context, state) => const WalletPage(),
+        routes: [
+          GoRoute(
+            path: AppRoute.enableBiometryAfterOnboarding.path,
+            builder: (_, __) => const AddSeedEnableBiometryPage(),
+          ),
+        ],
       ),
     ],
   );
