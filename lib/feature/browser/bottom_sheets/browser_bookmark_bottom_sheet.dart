fe4ad3b55d8a96f7713775c99d07656c229f3316
nesquikm <drapolyuk@gmail.com>
2023-08-10 13:34:19 +0400

    refactor: autonomous BrowserBookmarkSheet

--- lib/feature/browser/bottom_sheets/browser_bookmark_bottom_sheet.dart
+++ lib/feature/browser/bottom_sheets/browser_bookmark_bottom_sheet.dart
@@ -1,36 +1,34 @@
+import 'package:app/app/service/service.dart';
+import 'package:app/data/models/models.dart';
+import 'package:app/di/di.dart';
+import 'package:app/feature/browser/browser.dart';
 import 'package:app/generated/generated.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
 
 /// Helper function to show [BrowserBookmarkSheet].
 Future<void> showBrowserBookmarkSheet({
   required BuildContext context,
-  required VoidCallback onSharePressed,
-  required VoidCallback onRenamePressed,
-  required VoidCallback onDeletePressed,
+  required BrowserBookmarkItem item,
 }) {
   return showCommonBottomSheet(
     title: LocaleKeys.browserBookmarkSettings.tr(),
     context: context,
     body: (_, __) => BrowserBookmarkSheet(
-      onSharePressed: onSharePressed,
-      onRenamePressed: onRenamePressed,
-      onDeletePressed: onDeletePressed,
+      item: item,
     ),
   );
 }
 
 class BrowserBookmarkSheet extends StatefulWidget {
   const BrowserBookmarkSheet({
-    required this.onSharePressed,
-    required this.onRenamePressed,
-    required this.onDeletePressed,
+    required this.item,
     super.key,
   });
 
-  final VoidCallback onSharePressed;
-  final VoidCallback onRenamePressed;
-  final VoidCallback onDeletePressed;
+  final BrowserBookmarkItem item;
 
   @override
   State<BrowserBookmarkSheet> createState() => _BrowserBookmarkSheetState();
@@ -41,51 +39,64 @@ class _BrowserBookmarkSheetState extends State<BrowserBookmarkSheet> {
   Widget build(BuildContext context) {
     final colors = context.themeStyle.colors;
 
-    return ShapedContainerColumn(
-      margin: EdgeInsets.zero,
-      mainAxisSize: MainAxisSize.min,
-      separator: const CommonDivider(),
-      children: [
-        CommonListTile(
-          onPressed: _onSharePressed,
-          titleText: LocaleKeys.browserBookmarkShare.tr(),
-          trailing: CommonIconWidget.svg(
-            svg: Assets.images.share.path,
-            color: colors.textPrimary,
-          ),
-        ),
-        CommonListTile(
-          onPressed: _onRenamePressed,
-          titleText: LocaleKeys.browserBookmarkRename.tr(),
-          trailing: CommonIconWidget.svg(
-            svg: Assets.images.edit.path,
-            color: colors.textPrimary,
-          ),
-        ),
-        CommonListTile(
-          onPressed: _onDeletePressed,
-          titleText: LocaleKeys.browserBookmarkDelete.tr(),
-          trailing: CommonIconWidget.svg(
-            svg: Assets.images.trash.path,
-          ),
-          contentColor: colors.alert,
-        ),
-      ],
+    return BlocProvider<BrowserBookmarksBloc>(
+      create: (context) => BrowserBookmarksBloc(
+        inject<BrowserBookmarksStorageService>(),
+      ),
+      child: Builder(
+        builder: (context) {
+          return ShapedContainerColumn(
+            margin: EdgeInsets.zero,
+            mainAxisSize: MainAxisSize.min,
+            separator: const CommonDivider(),
+            children: [
+              CommonListTile(
+                onPressed: () => _onSharePressed(context),
+                titleText: LocaleKeys.browserBookmarkShare.tr(),
+                trailing: CommonIconWidget.svg(
+                  svg: Assets.images.share.path,
+                  color: colors.textPrimary,
+                ),
+              ),
+              CommonListTile(
+                onPressed: () => _onRenamePressed(context),
+                titleText: LocaleKeys.browserBookmarkRename.tr(),
+                trailing: CommonIconWidget.svg(
+                  svg: Assets.images.edit.path,
+                  color: colors.textPrimary,
+                ),
+              ),
+              CommonListTile(
+                onPressed: () => _onDeletePressed(context),
+                titleText: LocaleKeys.browserBookmarkDelete.tr(),
+                trailing: CommonIconWidget.svg(
+                  svg: Assets.images.trash.path,
+                ),
+                contentColor: colors.alert,
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 
-  void _onSharePressed() {
+  void _onSharePressed(BuildContext context) {
     Navigator.of(context).pop();
-    widget.onSharePressed.call();
+    final shareText = widget.item.url.toString();
+    Share.share(shareText);
   }
 
-  void _onRenamePressed() {
+  void _onRenamePressed(BuildContext context) {
     Navigator.of(context).pop();
-    widget.onRenamePressed.call();
   }
 
-  void _onDeletePressed() {
+  void _onDeletePressed(BuildContext context) {
     Navigator.of(context).pop();
-    widget.onDeletePressed.call();
+    context.read<BrowserBookmarksBloc>().add(
+          BrowserBookmarksEvent.remove(
+            id: widget.item.id,
+          ),
+        );
   }
 }
--- lib/feature/browser/browser_tabs_view/browser_start_view.dart
+++ lib/feature/browser/browser_tabs_view/browser_start_view.dart
@@ -7,7 +7,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
 
 /// Item count limit for each section
@@ -174,28 +173,11 @@ class _BrowserStartViewState extends State<BrowserStartView> {
 
     showBrowserBookmarkSheet(
       context: context,
-      onSharePressed: () => _onSharePressed(item),
-      onRenamePressed: () => _onRenamePressed(item),
-      onDeletePressed: () => _onDeletePressed(item),
+      item: item,
     );
   }
 
   void _onSeeAllPressed() {
     context.goNamed(AppRoute.browserBookmarks.name);
-  }
-
-  void _onSharePressed(BrowserBookmarkItem item) {
-    final shareText = item.url.toString();
-    Share.share(shareText);
-  }
-
-  void _onRenamePressed(BrowserBookmarkItem item) {}
-
-  void _onDeletePressed(BrowserBookmarkItem item) {
-    context.read<BrowserBookmarksBloc>().add(
-          BrowserBookmarksEvent.remove(
-            id: item.id,
-          ),
-        );
   }
 }
