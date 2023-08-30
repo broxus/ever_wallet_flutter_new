bc50a4b4a9f32131fd25f48fbde2ab05761fab8b
Alex Adrianov <32712725+Alex-A4@users.noreply.github.com>
2023-08-30 16:49:54 +0300

    feat: allow copying transaction fields (#243)
    * feat: update nekoton and change jrpc to proto
    
    * feat: allow copying some values from transaction detail:
    
    ---------
    
    Co-authored-by: nesquikm <drapolyuk@gmail.com>

--- lib/feature/wallet/widgets/account_transactions_tab/detail/details_item.dart
+++ lib/feature/wallet/widgets/account_transactions_tab/detail/details_item.dart
@@ -1,4 +1,8 @@
+import 'package:app/app/service/service.dart';
+import 'package:app/di/di.dart';
+import 'package:app/generated/generated.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
 
 /// Widget that displays title and content of transaction information.
@@ -9,6 +13,8 @@ class TonWalletTransactionDetailsItem extends StatelessWidget {
     this.contentChild,
     super.key,
     this.titleTrailingChild,
+    this.copyMessage,
+    this.copyValue,
   }) : assert(
           content == null && contentChild != null ||
               content != null && contentChild == null,
@@ -28,9 +34,16 @@ class TonWalletTransactionDetailsItem extends StatelessWidget {
   /// Typically, this is [MoneyWidget].
   final Widget? contentChild;
 
+  /// Value that should be copied to clipboard
+  final String? copyValue;
+
+  /// Message that will be displayed during copy action
+  final String? copyMessage;
+
   @override
   Widget build(BuildContext context) {
     final colors = context.themeStyle.colors;
+    final canCopy = copyValue != null;
 
     Widget titleWidget = Text(
       title,
@@ -50,19 +63,49 @@ class TonWalletTransactionDetailsItem extends StatelessWidget {
       );
     }
 
-    return SeparatedColumn(
+    final child = SeparatedColumn(
       separatorSize: DimensSize.d4,
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         titleWidget,
-        if (content != null)
-          Text(
-            content!,
-            style: StyleRes.primaryBold.copyWith(color: colors.textPrimary),
-          ),
-        if (contentChild != null) contentChild!,
+        SeparatedRow(
+          separatorSize: DimensSize.d16,
+          children: [
+            if (content != null)
+              Expanded(
+                child: Text(
+                  content!,
+                  style: StyleRes.primaryBold.copyWith(
+                    color: canCopy ? colors.blue : colors.textPrimary,
+                  ),
+                ),
+              ),
+            if (contentChild != null) Expanded(child: contentChild!),
+            if (canCopy)
+              CommonIconWidget.svg(
+                svg: Assets.images.copy.path,
+                color: colors.blue,
+              ),
+          ],
+        ),
       ],
     );
+
+    if (canCopy && copyValue != null) {
+      return PressScaleWidget(
+        onLongPress: () {
+          Clipboard.setData(ClipboardData(text: copyValue!));
+          if (copyMessage != null) {
+            inject<MessengerService>().show(
+              Message.successful(message: copyMessage!),
+            );
+          }
+        },
+        child: child,
+      );
+    }
+
+    return child;
   }
 }
