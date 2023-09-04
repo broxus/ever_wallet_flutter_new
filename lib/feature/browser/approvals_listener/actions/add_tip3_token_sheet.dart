7584d6f11e98f512f59292b0b366a85950a0f300
Alex Adrianov <32712725+Alex-A4@users.noreply.github.com>
2023-09-04 15:39:46 +0300

    feat: implement approvals listener (#255)

---
+++ lib/feature/browser/approvals_listener/actions/add_tip3_token_sheet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:nekoton_repository/nekoton_repository.dart';
+
+/// Helper function that allows show sheet to add some token with [details] to
+/// [account].
+// ignore_for_file: avoid-unused-parameters
+Future<bool> showAddTip3TokenSheet({
+  required BuildContext context,
+  required Uri origin,
+  required Address account,
+  required RootTokenContractDetails details,
+}) {
+  return Future.value(false);
+}
