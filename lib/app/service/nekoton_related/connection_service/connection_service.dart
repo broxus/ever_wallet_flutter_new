2be2b7b9fd40ce715e420255cfa455bb87cb65c3
nesquikm <drapolyuk@gmail.com>
2023-09-19 17:44:59 +0400

    fix: remove networkId

--- lib/app/service/nekoton_related/connection_service/connection_service.dart
+++ lib/app/service/nekoton_related/connection_service/connection_service.dart
@@ -43,29 +43,26 @@ class ConnectionService {
   Future<void> _updateTransportByConnection(ConnectionData connection) async {
     _log.finest('updateTransportByConnection: ${connection.name}');
     final transport = await connection.when<Future<Transport>>(
-      gql: (_, name, networkId, group, endpoints, timeout, __, isLocal, ___) =>
+      gql: (_, name, group, endpoints, timeout, __, isLocal, ___) =>
           _nekotonRepository.createGqlTransport(
         post: _httpService.postTransportData,
         get: _httpService.getTransportData,
         name: name,
-        networkId: networkId,
         group: group,
         endpoints: endpoints,
         local: isLocal,
       ),
-      proto: (_, name, networkId, group, endpoint, __, ___) =>
+      proto: (_, name, group, endpoint, __, ___) =>
           _nekotonRepository.createProtoTransport(
         post: _httpService.postTransportDataBytes,
         name: name,
-        networkId: networkId,
         group: group,
         endpoint: endpoint,
       ),
-      jrpc: (_, name, networkId, group, endpoint, __, ___) =>
+      jrpc: (_, name, group, endpoint, __, ___) =>
           _nekotonRepository.createJrpcTransport(
         post: _httpService.postTransportData,
         name: name,
-        networkId: networkId,
         group: group,
         endpoint: endpoint,
       ),
--- lib/app/service/nekoton_related/connection_service/network_presets.dart
+++ lib/app/service/nekoton_related/connection_service/network_presets.dart
@@ -5,12 +5,12 @@ const defaultNetworkTimeout = Duration(milliseconds: 60000);
 
 const _everMainnetProtoId = 'preset_ever_mainnet_proto';
 
-const defaultNetworkId = _everMainnetProtoId;
+const defaultConnectionkId = _everMainnetProtoId;
 
 final networkPresets = [..._everPresets, ..._venomPresets, ..._customPresets];
 
 final defaultNetwork = networkPresets.firstWhere(
-  (element) => element.id == defaultNetworkId,
+  (element) => element.id == defaultConnectionkId,
 );
 
 /// Presets for ever network
@@ -18,7 +18,6 @@ final _everPresets = <ConnectionData>[
   ConnectionData.protoPreset(
     id: _everMainnetProtoId,
     name: 'Mainnet (PROTO)',
-    networkId: 1,
     group: 'mainnet',
     endpoint: 'https://jrpc.everwallet.net/proto',
     networkType: NetworkType.ever,
@@ -26,7 +25,6 @@ final _everPresets = <ConnectionData>[
   ConnectionData.gqlPreset(
     id: 'preset_ever_mainnet_gql',
     name: 'Mainnet (GQL)',
-    networkId: 1,
     group: 'mainnet',
     endpoints: [
       'https://mainnet.evercloud.dev/89a3b8f46a484f2ea3bdd364ddaee3a3/graphql',
@@ -37,7 +35,6 @@ final _everPresets = <ConnectionData>[
   ConnectionData.gqlPreset(
     id: 'preset_ever_testnet_gql',
     name: 'Testnet',
-    networkId: 2,
     group: 'testnet',
     endpoints: [
       'https://devnet.evercloud.dev/89a3b8f46a484f2ea3bdd364ddaee3a3/graphql',
@@ -61,7 +58,6 @@ final _venomPresets = [
   ConnectionData.protoPreset(
     id: 'preset_venom_testnet_proto',
     name: 'Testnet Venom',
-    networkId: 1010,
     group: 'testnet',
     endpoint: 'https://jrpc-testnet.venom.foundation/proto',
     networkType: NetworkType.venom,
@@ -73,7 +69,6 @@ final _customPresets = <ConnectionData>[
   ConnectionData.gqlPreset(
     id: 'preset_tonlabs_mainnet_gql',
     name: 'fld.ton.dev',
-    networkId: 10,
     group: 'fld',
     endpoints: [
       'https://gql.custler.net/graphql',
@@ -84,7 +79,6 @@ final _customPresets = <ConnectionData>[
   ConnectionData.gqlPreset(
     id: 'preset_tonlabs_testnet_gql',
     name: 'Gosh',
-    networkId: 30,
     group: 'gosh',
     endpoints: [
       'https://network.gosh.sh',
