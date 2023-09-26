169fed40721987b12394888c14a122a7cf0b095b
nesquikm <drapolyuk@gmail.com>
2023-09-26 14:01:15 +0400

    feat: actions added

--- lib/data/models/connection_data.dart
+++ lib/data/models/connection_data.dart
@@ -1,3 +1,4 @@
+import 'package:app/app/service/nekoton_related/connection_service/network_presets.dart';
 import 'package:app/data/models/network_type.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:uuid/uuid.dart';
@@ -26,19 +27,20 @@ sealed class ConnectionData with _$ConnectionData {
     required String name,
     required String group,
     required List<String> endpoints,
-    required Duration timeout,
     required NetworkType networkType,
     required bool isLocal,
     required String blockExplorerUrl,
     required String manifestUrl,
     required String nativeTokenTicker,
+    Duration? timeout,
+    String? id,
   }) =>
       ConnectionData.gql(
-        id: const Uuid().v4(),
+        id: id ?? const Uuid().v4(),
         name: name,
         group: group,
         endpoints: endpoints,
-        timeout: timeout,
+        timeout: timeout ?? defaultNetworkTimeout,
         networkType: networkType,
         isLocal: isLocal,
         blockExplorerUrl: blockExplorerUrl,
@@ -95,9 +97,10 @@ sealed class ConnectionData with _$ConnectionData {
     required String blockExplorerUrl,
     required String manifestUrl,
     required String nativeTokenTicker,
+    String? id,
   }) =>
       ConnectionData.proto(
-        id: const Uuid().v4(),
+        id: id ?? const Uuid().v4(),
         name: name,
         group: group,
         endpoint: endpoint,
@@ -153,9 +156,10 @@ sealed class ConnectionData with _$ConnectionData {
     required String blockExplorerUrl,
     required String manifestUrl,
     required String nativeTokenTicker,
+    String? id,
   }) =>
       ConnectionData.jrpc(
-        id: const Uuid().v4(),
+        id: id ?? const Uuid().v4(),
         name: name,
         group: group,
         endpoint: endpoint,
