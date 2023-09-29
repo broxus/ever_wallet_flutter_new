ff59b75d9fd5e53c9c18268455c54320ecd26983
nesquikm <drapolyuk@gmail.com>
2023-09-29 11:00:18 +0400

    docs: some comments

--- lib/bootstrap/ntp_service.dart
+++ lib/bootstrap/ntp_service.dart
@@ -2,7 +2,7 @@ import 'package:app/app/service/service.dart';
 import 'package:app/di/di.dart';
 import 'package:logging/logging.dart';
 
-/// Initialize storages and its streams
+/// NTP service which provides the correct time of the system clock with offset
 Future<void> configureNtpService() async {
   final log = Logger('bootstrap')..finest('NtpService initializating...');
   await inject<NtpService>().init();
