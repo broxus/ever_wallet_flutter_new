d2b8c02fb92e0ea52c3dd27310ca6e0501f491b8
AlexA4 <alex.a4.25@yandex.ru>
2023-10-12 13:10:47 +0300

    fix: onboarding centering

--- lib/feature/onboarding/view/onboarding_view.dart
+++ lib/feature/onboarding/view/onboarding_view.dart
@@ -21,93 +21,98 @@ class OnboardingView extends StatelessWidget {
 
     return SafeArea(
       minimum: const EdgeInsets.only(bottom: DimensSize.d16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: DimensSize.d16,
-              vertical: DimensSize.d12,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const ChangeLanguageButton(),
-                CommonIconButton.svg(
-                  svg: Assets.images.support.path,
-                  buttonType: EverButtonType.secondary,
-                  size: CommonIconButtonSize.small,
-                  onPressed: () => showContactSupportSheet(
-                    context: context,
-                    mode: ContactSupportMode.initiatedByUser,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: DimensSize.d16,
+                vertical: DimensSize.d12,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const ChangeLanguageButton(),
+                  CommonIconButton.svg(
+                    svg: Assets.images.support.path,
+                    buttonType: EverButtonType.secondary,
+                    size: CommonIconButtonSize.small,
+                    onPressed: () => showContactSupportSheet(
+                      context: context,
+                      mode: ContactSupportMode.initiatedByUser,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: DimensSize.d12),
-          const Expanded(child: SlidingBlockChains()),
-          const SizedBox(height: DimensSize.d20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: DimensSize.d16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: DimensSize.d12),
-                Text(
-                  LocaleKeys.welcomeTitle.tr(),
-                  style: StyleRes.balance.copyWith(color: colors.textPrimary),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: DimensSize.d12),
-                Text(
-                  LocaleKeys.supportTokenAndAccessEverscale.tr(),
-                  style: StyleRes.primaryRegular
-                      .copyWith(color: colors.textPrimary),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: DimensSize.d24),
-                CommonButton.primary(
-                  text: LocaleKeys.createNewWallet.tr(),
-                  onPressed: () => context.goFurther(AppRoute.createSeed.path),
-                  fillWidth: true,
-                ),
-                const SizedBox(height: DimensSize.d8),
-                CommonButton.secondary(
-                  text: LocaleKeys.signInWithPhrase.tr(),
-                  onPressed: () => context.goFurther(AppRoute.enterSeed.path),
-                  fillWidth: true,
-                ),
-                // const SizedBox(height: 12),
-                // CommonButton.secondary(
-                // TODO(alex-a4): change icon
-                //   leading: Assets.images.ledger.svg(
-                //     color: style.styles.secondaryButtonStyle.color,
-                //   ),
-                //   onPressed: () {},
-                // ),
-                const SizedBox(height: DimensSize.d16),
-                Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: LocaleKeys.byProcessingAcceptLicense.tr(),
-                        style: StyleRes.addRegular
-                            .copyWith(color: colors.textSecondary),
-                      ),
-                      TextSpan(
-                        text: LocaleKeys.readHere.tr(),
-                        style: StyleRes.addRegular.copyWith(color: colors.blue),
-                        recognizer: TapGestureRecognizer()..onTap = _onLinkTap,
-                      ),
-                    ],
+            const SizedBox(height: DimensSize.d12),
+            const Expanded(child: SlidingBlockChains()),
+            const SizedBox(height: DimensSize.d20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: DimensSize.d16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: DimensSize.d12),
+                  Text(
+                    LocaleKeys.welcomeTitle.tr(),
+                    style: StyleRes.balance.copyWith(color: colors.textPrimary),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                  const SizedBox(height: DimensSize.d12),
+                  Text(
+                    LocaleKeys.supportTokenAndAccessEverscale.tr(),
+                    style: StyleRes.primaryRegular
+                        .copyWith(color: colors.textPrimary),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: DimensSize.d24),
+                  CommonButton.primary(
+                    text: LocaleKeys.createNewWallet.tr(),
+                    onPressed: () =>
+                        context.goFurther(AppRoute.createSeed.path),
+                    fillWidth: true,
+                  ),
+                  const SizedBox(height: DimensSize.d8),
+                  CommonButton.secondary(
+                    text: LocaleKeys.signInWithPhrase.tr(),
+                    onPressed: () => context.goFurther(AppRoute.enterSeed.path),
+                    fillWidth: true,
+                  ),
+                  // const SizedBox(height: 12),
+                  // CommonButton.secondary(
+                  // TODO(alex-a4): change icon
+                  //   leading: Assets.images.ledger.svg(
+                  //     color: style.styles.secondaryButtonStyle.color,
+                  //   ),
+                  //   onPressed: () {},
+                  // ),
+                  const SizedBox(height: DimensSize.d16),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: LocaleKeys.byProcessingAcceptLicense.tr(),
+                          style: StyleRes.addRegular
+                              .copyWith(color: colors.textSecondary),
+                        ),
+                        TextSpan(
+                          text: LocaleKeys.readHere.tr(),
+                          style:
+                              StyleRes.addRegular.copyWith(color: colors.blue),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = _onLinkTap,
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
