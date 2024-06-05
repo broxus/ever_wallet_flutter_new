e041a6d8c66c0408ad25bbacd31625d7fe5641d7
dmitriiandriianov <d.andriianov@rapty.health>
2024-06-06 01:53:24 +0700

    first deploy apk before ipa

--- scripts/build-deploy-fad.sh
+++ scripts/build-deploy-fad.sh
@@ -5,10 +5,17 @@ set -o pipefail
 source scripts/get-build-number.sh
 source scripts/get-changelog.sh
 
-source scripts/build-binary/fad-ipa.sh
 source scripts/build-binary/fad-apk.sh
-
-source scripts/deploy/fad-ipa.sh
 source scripts/deploy/fad-apk.sh
+
+source scripts/build-binary/fad-ipa.sh
+source scripts/deploy/fad-ipa.sh
+
+#Original:
+#source scripts/build-binary/fad-ipa.sh
+#source scripts/build-binary/fad-apk.sh
+#
+#source scripts/deploy/fad-ipa.sh
+#source scripts/deploy/fad-apk.sh
 
 
