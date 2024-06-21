720619fa0301e1c320b01fc676c881197bfcc4b1
dmitriiandriianov <d.andriianov@rapty.health>
2024-06-21 16:02:07 +0700

    Add codegen files to repository

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,11 +2,7 @@
 
 # Remove build files generated code
 find . -type f -name "*_generated.*" -delete
-find . -type f -name "*.freezed.dart" -delete
-find . -type f -name "*.g.dart" -delete
-find . -type f -name "*.gen.dart" -delete
 find . -type f -name "*.reflectable.dart" -delete
-find . -type f -name "*.module.dart" -delete
 
 # Remove DI config
 rm -rf lib/di/di.config.dart
