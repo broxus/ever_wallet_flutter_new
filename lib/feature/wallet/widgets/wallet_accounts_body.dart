f5882ae0c25a84a0aa636a3b6133581da687c663
Dmitry Andriyanov <knightsforce@users.noreply.github.com>
2024-06-25 12:21:10 +0700

    fix: position bottomsheet with assets (#398)
    * Bottomsheet location with assets on the wallet screen using Render metrics
    
    * Sort packages
    
    * pubspec.yaml render_metrics above rxdart
    
    * try..catch inside onMount WalletAccountActions
    
    ---------
    
    Co-authored-by: dmitriiandriianov <d.andriianov@rapty.health>

--- lib/feature/wallet/widgets/wallet_accounts_body.dart
+++ lib/feature/wallet/widgets/wallet_accounts_body.dart
@@ -2,6 +2,7 @@ import 'package:app/feature/wallet/wallet.dart';
 import 'package:app/feature/widgets/change_notifier_listener.dart';
 import 'package:flutter/material.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
+import 'package:render_metrics/render_metrics.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
 
 const walletAccountCardHeight = DimensSize.d220;
@@ -16,6 +17,7 @@ class WalletAccountsBody extends StatefulWidget {
     required this.currentAccount,
     required this.controller,
     required this.publicKey,
+    required this.onMount,
     super.key,
   });
 
@@ -23,12 +25,15 @@ class WalletAccountsBody extends StatefulWidget {
   final PublicKey publicKey;
   final KeyAccount? currentAccount;
   final PageController controller;
+  final ValueChanged<double> onMount;
 
   @override
   State<WalletAccountsBody> createState() => _WalletAccountsBodyState();
 }
 
 class _WalletAccountsBodyState extends State<WalletAccountsBody> {
+  final _renderId = 0;
+
   @override
   Widget build(BuildContext context) {
     final list = widget.accounts;
@@ -80,12 +85,29 @@ class _WalletAccountsBodyState extends State<WalletAccountsBody> {
             mainAxisSize: MainAxisSize.min,
             children: [
               _pageIndicators(list.length + 1),
-              WalletAccountActions(currentAccount: widget.currentAccount),
+              RenderMetricsObject(
+                id: _renderId,
+                onMount: _onUpdate,
+                onUpdate: _onUpdate,
+                child: WalletAccountActions(
+                  currentAccount: widget.currentAccount,
+                ),
+              ),
             ],
           ),
         ),
       ],
     );
+  }
+
+  void _onUpdate(_, RenderMetricsBox rb) {
+    Future.delayed(const Duration(milliseconds: 100), () {
+      try {
+        widget.onMount(rb.data.bottomCenter.y);
+      } on Object catch (e) {
+        debugPrint(e.toString());
+      }
+    });
   }
 
   Widget _pageIndicators(int count) {
