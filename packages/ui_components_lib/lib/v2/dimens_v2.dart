fd5b9d22033001fda21bd845ec8dcc88e42dd14b
Andrey Malochka <andrey.molocko2018@gmail.com>
2024-07-15 11:51:52 +0300

    fix: updated theme textstyles and dimens

--- packages/ui_components_lib/lib/v2/dimens_v2.dart
+++ packages/ui_components_lib/lib/v2/dimens_v2.dart
@@ -2,19 +2,19 @@ class DimensSizeV2 {
   const DimensSizeV2._();
 
   static const double d0 = 0;
-  static const double d2 = 8;
-  static const double d4 = 12;
-  static const double d8 = 16;
-  static const double d12 = 20;
-  static const double d16 = 24;
-  static const double d20 = 28;
-  static const double d24 = 32;
-  static const double d28 = 36;
-  static const double d32 = 40;
-  static const double d36 = 44;
-  static const double d40 = 48;
-  static const double d44 = 56;
-  static const double d48 = 64;
+  static const double d2 = 2;
+  static const double d4 = 4;
+  static const double d8 = 8;
+  static const double d12 = 12;
+  static const double d16 = 16;
+  static const double d20 = 20;
+  static const double d24 = 24;
+  static const double d28 = 28;
+  static const double d32 = 32;
+  static const double d36 = 36;
+  static const double d40 = 40;
+  static const double d44 = 44;
+  static const double d48 = 48;
 }
 
 class DimensStrokeV2 {
