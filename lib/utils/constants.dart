1ea25544d30b33d2e38d6b1d2933714e465c5756
Egor Komarov <Odrin@users.noreply.github.com>
2024-07-30 20:57:28 +0200

    feat(EWM-223): estimate min attached amount (#426)
    Co-authored-by: Egor Komarov <egor.komarov@bf.rocks>

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,5 @@
 import 'package:nekoton_repository/nekoton_repository.dart';
 
-/// Default attach amount to transaction in native tokens (for example, 3 EVER)
-final defaultAttachAmount = BigInt.parse('3000000000');
-
 /// Default bounce flag for sending transaction
 const defaultMessageBounce = false;
 
