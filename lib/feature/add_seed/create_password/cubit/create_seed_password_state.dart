2c28003fdd9059de4b00cae66d5db1f325b4aa42
Dmitry Andriyanov <knightsforce@users.noreply.github.com>
2024-08-15 14:28:11 +0300

    EWM-228. SecureTextField (#435)

--- lib/feature/add_seed/create_password/cubit/create_seed_password_state.dart
+++ lib/feature/add_seed/create_password/cubit/create_seed_password_state.dart
@@ -4,15 +4,11 @@ part of 'create_seed_password_cubit.dart';
 @freezed
 class CreateSeedPasswordState with _$CreateSeedPasswordState {
   const factory CreateSeedPasswordState({
-    required bool obscurePassword,
-    required bool obscureConfirm,
     required bool isLoading,
     required PasswordStatus status,
   }) = _CreateSeedPasswordState;
 
   factory CreateSeedPasswordState.initial() => const CreateSeedPasswordState(
-        obscurePassword: true,
-        obscureConfirm: true,
         isLoading: false,
         status: PasswordStatus.initial,
       );
