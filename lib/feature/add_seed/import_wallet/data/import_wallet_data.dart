19f0f947913b5f4d41af4ff52e7fdd882d1cfebb
Andrey Molochko <36672245+AndreyMolochko@users.noreply.github.com>
2024-08-22 18:54:36 +0300

    fix: ewm-221 add segment controls with words (#445)
    * bug(ewm-221) add segment controls with words
    
    * fix linter
    
    * fix linter
    
    * fix comment
    
    ---------
    
    Co-authored-by: Andrey Malochka <andrey.molocko2018@gmail.com>

--- lib/feature/add_seed/import_wallet/data/import_wallet_data.dart
+++ lib/feature/add_seed/import_wallet/data/import_wallet_data.dart
@@ -1,24 +1,32 @@
 class ImportWalletData {
   ImportWalletData({
     this.isPasted,
+    this.allowedData,
+    this.selectedValue,
     this.words,
     this.firstColumnWords,
     this.secondColumnWords,
   });
 
   final bool? isPasted;
+  final List<int>? allowedData;
+  final int? selectedValue;
   final List<String>? words;
   final List<String>? firstColumnWords;
   final List<String>? secondColumnWords;
 
   ImportWalletData copyWith({
     bool? isPasted,
+    List<int>? allowedData,
+    int? selectedValue,
     List<String>? words,
     List<String>? firstColumnWords,
     List<String>? secondColumnWords,
   }) {
     return ImportWalletData(
       isPasted: isPasted ?? this.isPasted,
+      allowedData: allowedData ?? this.allowedData,
+      selectedValue: selectedValue ?? this.selectedValue,
       words: words,
       firstColumnWords: firstColumnWords,
       secondColumnWords: secondColumnWords,
