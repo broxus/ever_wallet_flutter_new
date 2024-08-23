1f851139de1a6b7158c2d419afebfefc03fc7eb8
knightforce <andrianov44@gmail.com>
2024-08-23 22:44:24 +0200

    EWM-241. Refactor BrowserPrimarySheet

--- lib/feature/browser/bottom_sheets/browser_primary_bottom_sheet.dart
+++ lib/feature/browser/bottom_sheets/browser_primary_bottom_sheet.dart
@@ -3,6 +3,7 @@ import 'package:app/generated/generated.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
+import 'package:ui_components_lib/v2/dimens_v2.dart';
 
 /// Helper function to show [BrowserPrimarySheet].
 Future<void> showBrowserPrimarySheet({
@@ -31,6 +32,7 @@ class BrowserPrimarySheet extends StatelessWidget {
     required this.onAddToBookmarkPressed,
     super.key,
   });
+
   final bool refreshEnabled;
   final bool addToBookmarksEnabled;
   final VoidCallback? onRefreshPressed;
@@ -38,49 +40,79 @@ class BrowserPrimarySheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = context.themeStyle.colors;
+    final colors = context.themeStyleV2.colors;
 
     return ShapedContainerColumn(
+      color: colors.background2,
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
       mainAxisSize: MainAxisSize.min,
       separator: const CommonDivider(),
-      margin: EdgeInsets.zero,
       children: [
         if (refreshEnabled)
-          CommonListTile(
+          _Tile(
             titleText: LocaleKeys.browserRefresh.tr(),
             onPressed: () {
               Navigator.of(context).pop();
               onRefreshPressed?.call();
             },
-            contentColor: colors.textPrimary,
             trailing: CommonButtonIconWidget.svg(
               svg: Assets.images.refresh.path,
             ),
+            backgroundColor: colors.background2,
           ),
         if (addToBookmarksEnabled)
-          CommonListTile(
+          _Tile(
             titleText: LocaleKeys.browserAddToBookmarks.tr(),
             onPressed: () {
               Navigator.of(context).pop();
               onAddToBookmarkPressed?.call();
             },
-            contentColor: colors.textPrimary,
             trailing: CommonButtonIconWidget.svg(
               svg: Assets.images.star.path,
             ),
+            backgroundColor: colors.background2,
           ),
-        CommonListTile(
+        _Tile(
           titleText: LocaleKeys.browserHistory.tr(),
           onPressed: () {
             Navigator.of(context).pop();
             context.goNamed(AppRoute.browserHistory.name);
           },
-          contentColor: colors.textPrimary,
           trailing: CommonButtonIconWidget.svg(
             svg: Assets.images.history.path,
           ),
+          backgroundColor: colors.background2,
         ),
       ],
+    );
+  }
+}
+
+class _Tile extends StatelessWidget {
+  const _Tile({
+    required this.titleText,
+    required this.onPressed,
+    required this.trailing,
+    required this.backgroundColor,
+  });
+
+  final String titleText;
+  final VoidCallback onPressed;
+  final Widget trailing;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonListTile(
+      titleText: titleText,
+      padding: const EdgeInsets.symmetric(
+        vertical: DimensSizeV2.d18,
+        horizontal: DimensSizeV2.d24,
+      ),
+      onPressed: onPressed,
+      backgroundColor: backgroundColor,
+      trailing: trailing,
     );
   }
 }
