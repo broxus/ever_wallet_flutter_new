3f06e21d1d41753066f72f96ea355a7b9587f854
knightforce <andrianov44@gmail.com>
2024-08-29 06:59:23 +0200

    echo "DEPLOY_TARGET is set to: $DEPLOY_TARGET"

--- scripts/ios-match-assure.sh
+++ scripts/ios-match-assure.sh
@@ -9,5 +9,7 @@ export MATCH_GIT_PRIVATE_KEY="../secrets/ios-provisioning-key"
 DEPLOY_TARGET=$1
 export DEPLOY_TARGET
 
+echo "DEPLOY_TARGET is set to: $DEPLOY_TARGET"
+
 cd ios
 fastlane ios match_assure
