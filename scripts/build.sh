f0eb064e19339dd9a5b41d47c586f204de8f5a9f
knightforce <andrianov44@gmail.com>
2024-09-04 08:33:54 +0700

    Get build number in parent workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,38 +4,37 @@ set -o pipefail
 
 export SENTRY_DSN
 
-source scripts/get-build-number.sh
+deploy_target="$1"
+build_number="$2"
+
+if [ -z "$build_number" ]; then
+    echo "Error: Build number is not specified."
+    exit 1
+fi
+
+export $BUILD_NUMBER_STRING="$build_number"
+
 source scripts/get-changelog.sh
 
-case "$1" in
+case "$deploy_target" in
   "fad"|"ios_fad"|"fad_store")
     source scripts/build-binary/fad-ipa.sh
     source scripts/deploy/fad-ipa.sh
     ;;
-esac
-
-case "$1" in
   "fad"|"android_fad"|"fad_store")
     source scripts/build-binary/fad-apk.sh
     source scripts/deploy/fad-apk.sh
     ;;
-esac
-
-case "$1" in
   "store"|"fad_store"|"ios_store")
     source scripts/build-binary/store-ipa.sh
     source scripts/deploy/store-ipa.sh
     ;;
-esac
-
-case "$1" in
   "store"|"fad_store"|"android_store")
     source scripts/build-binary/store-aab.sh
     source scripts/deploy/store-aab.sh
     ;;
-esac
-
-if [[ "$1" != "fad" && "$1" != "store" && "$1" != "fad_store" && "$1" != "ios_fad" && "$1" != "ios_store" && "$1" != "android_fad" && "$1" != "android_store" ]]; then
-    echo "Unknown deploy target: $1"
+  *)
+    echo "Unknown deploy target: $deploy_target"
     exit 1
-fi
+    ;;
+esac
