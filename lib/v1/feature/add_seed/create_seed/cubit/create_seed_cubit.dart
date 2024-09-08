ef8be138b140724a3d9f46251c6219efed4329bb
Dmitry Andriyanov <knightsforce@users.noreply.github.com>
2024-09-08 18:05:24 +0700

    feat: dev to test (#477)
    * feat(EWM-264): account balances (#474)
    
    Co-authored-by: Egor Komarov <egor.komarov@bf.rocks>
    
    * refactor: Remove language menu (#476)
    
    * app-deploy.yaml on push branches test
    
    * Add branch-workflow.yaml
    
    * melos.yaml add deploy_ios, deploy_android, deploy_fad commands
    
    * Update README.md
    
    * Rename to deploy_fad_ios and deploy_fad_android
    
    * Move get SENTRY_DSN to build.sh
    
    * build.sh deploy to store only from main
    
    * app-deploy.yaml store job check branch to deploy to stores
    
    * Hide language item from menu in profile
    
    * Update build flow
    
    * Update Readme Deploy from local machine section
    
    * Fix android-deploy orders
    
    * fix: Ewm 250 (#478)
    
    * EWM-250. Fix add seed
    
    * PopScope replace onPopInvoked -> onPopInvokedWithResult + addPostFrameCallback
    
    * Remove todo
    
    * add check didPop in onPopInvokedWithResult
    
    * Fix create new seed from profile
    
    * Fix analyzer issue
    
    * SeedPhraseModel
    
    ---------
    
    Co-authored-by: Egor Komarov <Odrin@users.noreply.github.com>
    Co-authored-by: Egor Komarov <egor.komarov@bf.rocks>

--- lib/v1/feature/add_seed/create_seed/cubit/create_seed_cubit.dart
+++ lib/v1/feature/add_seed/create_seed/cubit/create_seed_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:app/data/models/seed/seed_phrase_model.dart';
 import 'package:app/v1/feature/add_seed/constants.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/services.dart';
@@ -5,6 +6,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
 
 part 'create_seed_cubit.freezed.dart';
+
 part 'create_seed_state.dart';
 
 /// Cubit that helps generating seed phrase.
@@ -13,7 +15,12 @@ class CreateSeedCubit extends Cubit<CreateSeedCubitState> {
 
   Future<void> init() async {
     final seed = await generateKey(accountType: defaultMnemonicType);
-    emit(CreateSeedCubitState.generated(words: seed.words, isCopied: false));
+    emit(
+      CreateSeedCubitState.generated(
+        seed: SeedPhraseModel.fromWords(seed.words),
+        isCopied: false,
+      ),
+    );
   }
 
   Future<void> copySeed() async {
@@ -21,7 +28,7 @@ class CreateSeedCubit extends Cubit<CreateSeedCubitState> {
     if (st is _$GeneratedImpl) {
       emit(st.copyWith(isCopied: true));
       await Clipboard.setData(
-        ClipboardData(text: st.words.join(' ')),
+        ClipboardData(text: st.seed.phrase),
       );
       Future.delayed(const Duration(seconds: 2), () {
         emit(st.copyWith(isCopied: false));
--- lib/v1/feature/add_seed/create_seed/view/create_seed_view.dart
+++ lib/v1/feature/add_seed/create_seed/view/create_seed_view.dart
@@ -1,3 +1,4 @@
+import 'package:app/data/models/seed/seed_phrase_model.dart';
 import 'package:app/generated/generated.dart';
 import 'package:app/v1/feature/add_seed/create_seed/create_seed.dart';
 import 'package:collection/collection.dart';
@@ -8,7 +9,7 @@ import 'package:ui_components_lib/ui_components_lib.dart';
 import 'package:ui_components_lib/v2/ui_components_lib_v2.dart';
 
 /// Callback that calls if user taps skip checking or check phrase.
-typedef CreateSeedCompleteCallback = void Function(List<String> phrase);
+typedef CreateSeedCompleteCallback = ValueChanged<SeedPhraseModel>;
 
 /// {@template create_seed_view}
 /// Widget that allows user to create random seed phrase, copy it and check
@@ -53,11 +54,11 @@ class CreateSeedView extends StatelessWidget {
                       builder: (context, state) {
                         return state.when(
                           initial: Container.new,
-                          generated: (words, isCopied) {
+                          generated: (seed, isCopied) {
                             return Column(
                               mainAxisSize: MainAxisSize.min,
                               children: [
-                                _wordsField(words),
+                                _wordsField(seed),
                                 const SizedBox(height: DimensSizeV2.d4),
                                 _copyButton(isCopied),
                               ],
@@ -73,9 +74,9 @@ class CreateSeedView extends StatelessWidget {
             ),
             BlocBuilder<CreateSeedCubit, CreateSeedCubitState>(
               builder: (context, state) {
-                final words = state.maybeWhen(
-                  generated: (words, _) => words,
-                  orElse: () => const <String>[],
+                final seed = state.maybeWhen(
+                  generated: (seed, _) => seed,
+                  orElse: SeedPhraseModel.empty,
                 );
 
                 return Column(
@@ -83,13 +84,13 @@ class CreateSeedView extends StatelessWidget {
                     AccentButton(
                       buttonShape: ButtonShape.pill,
                       title: LocaleKeys.checkSeedPhrase.tr(),
-                      onPressed: () => checkCallback(words),
+                      onPressed: () => checkCallback(seed),
                     ),
                     const SizedBox(height: DimensSizeV2.d8),
                     PrimaryButton(
                       buttonShape: ButtonShape.pill,
                       title: LocaleKeys.skipTakeRisk.tr(),
-                      onPressed: () => skipCallback(words),
+                      onPressed: () => skipCallback(seed),
                     ),
                     const SizedBox(height: DimensSizeV2.d12),
                   ],
@@ -140,14 +141,14 @@ class CreateSeedView extends StatelessWidget {
     );
   }
 
-  Widget _wordsField(List<String> words) {
-    final lengthHalf = words.length ~/ 2;
+  Widget _wordsField(SeedPhraseModel seed) {
+    final lengthHalf = seed.wordsCount ~/ 2;
 
     return SeparatedRow(
       children: [
         Expanded(
           child: SeparatedColumn(
-            children: words
+            children: seed.words
                 .getRange(0, lengthHalf)
                 .mapIndexed((i, word) => _textPair(word, i + 1))
                 .toList(),
@@ -155,8 +156,8 @@ class CreateSeedView extends StatelessWidget {
         ),
         Expanded(
           child: SeparatedColumn(
-            children: words
-                .getRange(lengthHalf, words.length)
+            children: seed.words
+                .getRange(lengthHalf, seed.wordsCount)
                 .mapIndexed(
                   (i, word) => _textPair(word, i + lengthHalf + 1),
                 )
