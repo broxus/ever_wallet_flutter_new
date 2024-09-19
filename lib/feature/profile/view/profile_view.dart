ebc9caa9033963a8f7d956a4bd97a6c690ad0ca4
Andrey Malochka <andrey.molocko2018@gmail.com>
2024-09-19 17:52:37 +0300

    fix: ewm-288 fix touch/face id

--- lib/feature/profile/view/profile_view.dart
+++ lib/feature/profile/view/profile_view.dart
@@ -5,6 +5,7 @@ import 'package:app/generated/assets.gen.dart';
 import 'package:app/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:elementary_helper/elementary_helper.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
@@ -118,7 +119,10 @@ class ProfileView extends StatelessWidget {
                   firstSource: isBiometryAvailable,
                   secondSource: isBiometryEnabled,
                   builder: (_, available, enabled) {
-                    if (available != true) return const SizedBox.shrink();
+                    if (available != true &&
+                        defaultTargetPlatform != TargetPlatform.iOS) {
+                      return const SizedBox.shrink();
+                    }
                     return _profileTile(
                       leadingIcon: Assets.images.fingerSmall.path,
                       title: LocaleKeys.biometryWord.tr(),
