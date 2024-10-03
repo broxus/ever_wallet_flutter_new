e856e4b1082c7390d90de6f1dc25f1b64b4bc6cf
Andrey Molochko <36672245+AndreyMolochko@users.noreply.github.com>
2024-10-03 15:11:37 +0300

    fix: ewm-330 (#561)
    Co-authored-by: Andrey Malochka <andrey.molocko2018@gmail.com>

--- lib/feature/wallet/widgets/wallet_app_bar/wallet_app_bar_widget.dart
+++ lib/feature/wallet/widgets/wallet_app_bar/wallet_app_bar_widget.dart
@@ -43,34 +43,24 @@ class WalletAppBarWidget extends ElementaryWidget<WalletAppBarWidgetModel>
                         walletTypeName: wm.getWalletTypeName(
                           account.account.tonWallet.contract,
                         ),
+                        onTap: wm.onSettings,
                       ),
                     ) ??
                     const SizedBox.shrink(),
               ),
             ),
-            SeparatedRow(
-              separatorSize: DimensSizeV2.d12,
-              children: [
-                FloatButton(
-                  buttonShape: ButtonShape.circle,
-                  buttonSize: ButtonSize.small,
-                  onPressed: wm.onNetwork,
-                  child: StateNotifierBuilder(
-                    listenableState: wm.connection,
-                    builder: (_, connection) =>
-                        connection?.let(
-                          (value) => NetworkIcon(type: value.networkType),
-                        ) ??
-                        const SizedBox.shrink(),
-                  ),
-                ),
-                FloatButton(
-                  buttonShape: ButtonShape.circle,
-                  buttonSize: ButtonSize.small,
-                  icon: LucideIcons.cog,
-                  onPressed: wm.onSettings,
-                ),
-              ],
+            FloatButton(
+              buttonShape: ButtonShape.circle,
+              buttonSize: ButtonSize.small,
+              onPressed: wm.onNetwork,
+              child: StateNotifierBuilder(
+                listenableState: wm.connection,
+                builder: (_, connection) =>
+                    connection?.let(
+                      (value) => NetworkIcon(type: value.networkType),
+                    ) ??
+                    const SizedBox.shrink(),
+              ),
             ),
           ],
         ),
@@ -84,11 +74,13 @@ class _AccountInfo extends StatelessWidget {
     required this.account,
     required this.walletState,
     required this.walletTypeName,
+    required this.onTap,
   });
 
   final KeyAccount account;
   final TonWalletState? walletState;
   final String walletTypeName;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -97,65 +89,68 @@ class _AccountInfo extends StatelessWidget {
     final requiredConfirmations =
         walletState?.wallet?.details.requiredConfirmations;
 
-    return SeparatedRow(
-      children: [
-        UserAvatar(
-          address: account.address.address,
-        ),
-        Flexible(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                account.name,
-                style: theme.textStyles.headingXSmall,
-                overflow: TextOverflow.ellipsis,
-                softWrap: false,
-                maxLines: 1,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    LucideIcons.alignCenter,
-                    size: DimensSizeV2.d16,
-                    color: theme.colors.content3,
-                  ),
-                  const SizedBox(width: DimensSizeV2.d4),
-                  Flexible(
-                    child: Text(
-                      walletTypeName,
-                      style: theme.textStyles.labelXSmall.copyWith(
-                        color: theme.colors.content3,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: false,
-                      maxLines: 1,
+    return GestureDetector(
+      onTap: onTap,
+      child: SeparatedRow(
+        children: [
+          UserAvatar(
+            address: account.address.address,
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  account.name,
+                  style: theme.textStyles.headingXSmall,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: false,
+                  maxLines: 1,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      LucideIcons.alignCenter,
+                      size: DimensSizeV2.d16,
+                      color: theme.colors.content3,
                     ),
-                  ),
-                  const SizedBox(width: DimensSizeV2.d8),
-                  if (requiredConfirmations != null && custodians > 1)
-                    SeparatedRow(
-                      separatorSize: DimensSizeV2.d4,
-                      children: [
-                        Icon(
-                          LucideIcons.usersRound,
-                          size: DimensSizeV2.d16,
+                    const SizedBox(width: DimensSizeV2.d4),
+                    Flexible(
+                      child: Text(
+                        walletTypeName,
+                        style: theme.textStyles.labelXSmall.copyWith(
                           color: theme.colors.content3,
                         ),
-                        Text(
-                          '$requiredConfirmations/$custodians',
-                          style: theme.textStyles.labelXSmall.copyWith(
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: false,
+                        maxLines: 1,
+                      ),
+                    ),
+                    const SizedBox(width: DimensSizeV2.d8),
+                    if (requiredConfirmations != null && custodians > 1)
+                      SeparatedRow(
+                        separatorSize: DimensSizeV2.d4,
+                        children: [
+                          Icon(
+                            LucideIcons.usersRound,
+                            size: DimensSizeV2.d16,
                             color: theme.colors.content3,
                           ),
-                        ),
-                      ],
-                    ),
-                ],
-              ),
-            ],
+                          Text(
+                            '$requiredConfirmations/$custodians',
+                            style: theme.textStyles.labelXSmall.copyWith(
+                              color: theme.colors.content3,
+                            ),
+                          ),
+                        ],
+                      ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
