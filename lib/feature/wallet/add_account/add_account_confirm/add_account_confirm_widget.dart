c958e33d543f0c70ed25e984f03aa91e5da0a3b2
Egor Komarov <egor.komarov@bf.rocks>
2024-10-07 13:02:11 +0200

    fix: analyze

--- lib/feature/wallet/add_account/add_account_confirm/add_account_confirm_widget.dart
+++ lib/feature/wallet/add_account/add_account_confirm/add_account_confirm_widget.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:nekoton_repository/nekoton_repository.dart';
-import 'package:ui_components_lib/ui_components_lib.dart';
 import 'package:ui_components_lib/v2/ui_components_lib_v2.dart';
 
 class AddAccountConfirmWidget
