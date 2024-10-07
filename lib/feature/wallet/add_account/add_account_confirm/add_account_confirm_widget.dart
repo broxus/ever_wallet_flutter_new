a810110a1ab9fa8c487058f7a2242568e7f22f9b
Egor Komarov <egor.komarov@bf.rocks>
2024-10-07 12:50:47 +0200

    fix(EWM-341): add account info to confirm window

--- lib/feature/wallet/add_account/add_account_confirm/add_account_confirm_widget.dart
+++ lib/feature/wallet/add_account/add_account_confirm/add_account_confirm_widget.dart
@@ -1,4 +1,5 @@
 import 'package:app/feature/wallet/add_account/add_account_confirm/add_account_confirm_wm.dart';
+import 'package:app/feature/wallet/wallet.dart';
 import 'package:app/generated/generated.dart';
 import 'package:elementary/elementary.dart';
 import 'package:elementary_helper/elementary_helper.dart';
@@ -21,55 +22,56 @@ class AddAccountConfirmWidget
 
   @override
   Widget build(AddAccountConfirmWidgetModel wm) {
-    return SeparatedColumn(
-      separatorSize: DimensSizeV2.d28,
+    return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        if (wm.account != null)
+          AccountInfo(
+            account: wm.account!,
+            color: wm.theme.colors.background2,
+          ),
+        const SizedBox(height: DimensSizeV2.d16),
         SecureTextField(
           textEditingController: wm.controller,
           hintText: LocaleKeys.enterYourPassword.tr(),
           isAutofocus: true,
           onSubmit: (_) => wm.onPasswordSubmit(),
         ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AccentButton(
-              buttonShape: ButtonShape.pill,
-              title: LocaleKeys.confirm.tr(),
-              onPressed: wm.onPasswordSubmit,
-            ),
-            StateNotifierBuilder(
-              listenableState: wm.availableBiometry,
-              builder: (_, value) {
-                if (value?.contains(BiometricType.face) ?? false) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: DimensSizeV2.d8),
-                    child: PrimaryButton(
-                      buttonShape: ButtonShape.pill,
-                      title: LocaleKeys.useFaceID.tr(),
-                      icon: LucideIcons.scanFace,
-                      onPressed: wm.onUseBiometry,
-                    ),
-                  );
-                }
+        const SizedBox(height: DimensSizeV2.d28),
+        AccentButton(
+          buttonShape: ButtonShape.pill,
+          title: LocaleKeys.confirm.tr(),
+          onPressed: wm.onPasswordSubmit,
+        ),
+        StateNotifierBuilder(
+          listenableState: wm.availableBiometry,
+          builder: (_, value) {
+            if (value?.contains(BiometricType.face) ?? false) {
+              return Padding(
+                padding: const EdgeInsets.only(top: DimensSizeV2.d8),
+                child: PrimaryButton(
+                  buttonShape: ButtonShape.pill,
+                  title: LocaleKeys.useFaceID.tr(),
+                  icon: LucideIcons.scanFace,
+                  onPressed: wm.onUseBiometry,
+                ),
+              );
+            }
 
-                if (value?.contains(BiometricType.fingerprint) ?? false) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: DimensSizeV2.d8),
-                    child: PrimaryButton(
-                      buttonShape: ButtonShape.pill,
-                      title: LocaleKeys.useFingerprint.tr(),
-                      icon: LucideIcons.fingerprint,
-                      onPressed: wm.onUseBiometry,
-                    ),
-                  );
-                }
+            if (value?.contains(BiometricType.fingerprint) ?? false) {
+              return Padding(
+                padding: const EdgeInsets.only(top: DimensSizeV2.d8),
+                child: PrimaryButton(
+                  buttonShape: ButtonShape.pill,
+                  title: LocaleKeys.useFingerprint.tr(),
+                  icon: LucideIcons.fingerprint,
+                  onPressed: wm.onUseBiometry,
+                ),
+              );
+            }
 
-                return const SizedBox.shrink();
-              },
-            ),
-          ],
+            return const SizedBox.shrink();
+          },
         ),
       ],
     );
