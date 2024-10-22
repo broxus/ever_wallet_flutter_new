61a7f20daeeb51754b98bddf8f0a9a86f3e7bdc0
Dmitry Andriyanov <knightsforce@users.noreply.github.com>
2024-10-22 12:00:42 +0700

    fix: EWM-356 (#593)
    * EWM-356. Clipboard utils
    
    * EWM-356. Update regexp
    
    * EWM-356. Seed Easter egg

--- lib/feature/add_seed/enter_seed_phrase/cubit/enter_seed_phrase_cubit.dart
+++ lib/feature/add_seed/enter_seed_phrase/cubit/enter_seed_phrase_cubit.dart
@@ -6,6 +6,7 @@ import 'package:app/feature/add_seed/enter_seed_phrase/cubit/enter_seed_phrase_i
 import 'package:app/feature/constants.dart';
 import 'package:app/generated/locale_keys.g.dart';
 import 'package:app/utils/mixins/connection_mixin.dart';
+import 'package:app/utils/seed_utils.dart';
 import 'package:bloc/bloc.dart';
 import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -99,19 +100,13 @@ class EnterSeedPhraseCubit extends Cubit<EnterSeedPhraseState>
         if (st is _Tab && st.displayPasteButton == hasText) {
           emit(st.copyWith(displayPasteButton: !hasText));
         }
+
         if (hasText) {
           _checkDebugPhraseGenerating();
         }
 
         _checkInputCompletion(index);
       });
-    });
-    _controllers[0].addListener(() {
-      /// Only for 1-st controller allow paste as button
-      /// It's some bug but Input's paste removes spaces so check with length
-      if (_controllers[0].text.length > _debugPhraseLength) {
-        pastePhrase();
-      }
     });
     _focuses.forEachIndexed((index, f) {
       f.addListener(() => _checkInputCompletion(index));
@@ -239,11 +234,7 @@ class EnterSeedPhraseCubit extends Cubit<EnterSeedPhraseState>
   void clearInputModel(int index) => _controllers[index].clear();
 
   Future<void> pastePhrase() async {
-    final clipboard = await Clipboard.getData(Clipboard.kTextPlain);
-    final words = clipboard?.text
-            ?.replaceAll(RegExp(r'\\s+'), ' ')
-            .split(seedSplitRegExp) ??
-        <String>[];
+    final words = await getSeedListFromClipboard();
 
     if (words.isNotEmpty && words.length == _currentValue) {
       for (final word in words) {
