570f0c9e2873d813dad3cc24e6a06b6b06df78ce
knightforce <andrianov44@gmail.com>
2024-11-05 17:14:55 +0500

    TonService base methods

--- lib/app/service/ton/ton_service.dart
+++ lib/app/service/ton/ton_service.dart
@@ -1,4 +1,71 @@
+import 'package:darttonconnect/exceptions.dart';
+import 'package:darttonconnect/ton_connect.dart';
+import 'package:flutter/widgets.dart';
 import 'package:injectable/injectable.dart';
 
 @singleton
-class TonService {}
+class TonService {
+  static const _manifestUrl =
+      'https://l.sparxwallet.com/tonconnect-manifest.json';
+
+  final _connector = TonConnect(_manifestUrl);
+
+  void init() {
+    restoreConnection();
+  }
+
+  void connect() {
+    // WidgetsBiКnding.instance.addPostFrameCallback((_) {
+    //   if (!_connector.connected) {
+    //     restoreConnection();
+    //   }
+    // });
+  }
+
+  void disconnect() {
+    if (_connector.connected) {
+      _connector.disconnect();
+    }
+  }
+
+  void restoreConnection() => _connector.restoreConnection();
+
+  Future<void> sendTransaction() async {
+    if (!_connector.connected) {
+      return;
+    }
+
+    const transaction = <String, dynamic>{
+      // "validUntil": 1918097354,
+      // "messages": [
+      //   {
+      //     "address":
+      //         "0:575af9fc97311a11f423a1926e7fa17a93565babfd65fe39d2e58b8ccb38c911",
+      //     "amount": "20000000",
+      //   }
+      // ]
+    };
+
+    try {
+      await _connector.sendTransaction(transaction);
+    } on UserRejectsError catch (e) {
+      /// TODO
+    } catch (e) {
+      /// TODO
+    }
+  }
+
+  @disposeMethod
+  void dispose() {
+    disconnect();
+  }
+
+  // _() async {
+  //   final wallets = await _connector.getWallets();
+  //   final universalLink = await _connector.connect(walletConnectionSource);
+  //   updateQRCode(universalLink);
+  //   _connector.onStatusChange(onStatusChange);
+  // }
+
+  void onStatusChange(dynamic value) {}
+}
