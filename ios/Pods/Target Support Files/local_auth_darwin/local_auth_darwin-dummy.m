1d0ad2cea968e4b53ded4dbd9db76e4339a692f3
knightforce <andrianov44@gmail.com>
2024-11-12 11:53:13 +0500

    EWM-381. Update dependencies

--- ios/Pods/Target Support Files/local_auth_darwin/local_auth_darwin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_local_auth_darwin : NSObject
-@end
-@implementation PodsDummy_local_auth_darwin
-@end
--- ios/Pods/Target Support Files/mobile_scanner/mobile_scanner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mobile_scanner : NSObject
-@end
-@implementation PodsDummy_mobile_scanner
-@end
