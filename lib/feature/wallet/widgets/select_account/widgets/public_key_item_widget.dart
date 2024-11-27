d0c8dcd60dd8a7a0069104ae49342b2c0ee864fb
Andrey Molochko <36672245+AndreyMolochko@users.noreply.github.com>
2024-11-27 14:53:09 +0300

    fix: linter and fix scrolling selecting account (#646)
    * fix linter and fix scrolling selecting account
    
    * fix linter
    
    ---------
    
    Co-authored-by: Andrey Malochka <andrey.molocko2018@gmail.com>

--- lib/feature/wallet/widgets/select_account/widgets/public_key_item_widget.dart
+++ lib/feature/wallet/widgets/select_account/widgets/public_key_item_widget.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: inference_failure_on_function_return_type
+// ignore_for_file: lines_longer_than_80_chars
 import 'package:app/utils/utils.dart';
 import 'package:app/widgets/widgets.dart';
 import 'package:elementary_helper/elementary_helper.dart';
@@ -8,13 +9,14 @@ import 'package:nekoton_repository/nekoton_repository.dart';
 import 'package:ui_components_lib/ui_components_lib.dart';
 import 'package:ui_components_lib/v2/ui_components_lib_v2.dart';
 
-class PublicKeyItemWidget extends StatelessWidget {
+class PublicKeyItemWidget extends StatefulWidget {
   const PublicKeyItemWidget({
     required this.accounts,
     required this.currentAccount,
     required this.onTap,
     required this.getBalanceEntity,
     required this.scrollController,
+    required this.isScrollToAccount,
     super.key,
   });
 
@@ -23,45 +25,57 @@ class PublicKeyItemWidget extends StatelessWidget {
   final Function(KeyAccount) onTap;
   final ListenableState<Money> Function(KeyAccount) getBalanceEntity;
   final ScrollController scrollController;
+  final bool isScrollToAccount;
+
+  @override
+  State<PublicKeyItemWidget> createState() => _PublicKeyItemWidgetState();
+}
+
+class _PublicKeyItemWidgetState extends State<PublicKeyItemWidget> {
+  late final itemKeys = <KeyAccount, GlobalKey>{
+    for (final account in widget.accounts) account: GlobalKey(),
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.isScrollToAccount) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollToCurrentAccount(itemKeys);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = context.themeStyleV2;
 
-    final Map<KeyAccount, GlobalKey> itemKeys = {
-      for (var account in accounts) account: GlobalKey(),
-    };
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _scrollToCurrentAccount(itemKeys);
-    });
-
     return Column(
       children: [
-        for (var i = 0; i < accounts.length; i++)
+        for (var i = 0; i < widget.accounts.length; i++)
           GestureDetector(
-            key: itemKeys[accounts[i]],
-            onTap: () => onTap(accounts[i]),
+            key: itemKeys[widget.accounts[i]],
+            onTap: () => widget.onTap(widget.accounts[i]),
             child: Container(
               decoration: BoxDecoration(
-                color: accounts[i] == currentAccount
+                color: widget.accounts[i] == widget.currentAccount
                     ? theme.colors.background3
                     : theme.colors.background2,
               ),
               padding: EdgeInsets.only(
-                top: (i != 0 || accounts[i] == currentAccount)
+                top: (i != 0 || widget.accounts[i] == widget.currentAccount)
                     ? DimensSizeV2.d12
                     : 0,
-                bottom:
-                    (i < accounts.length - 1 || accounts[i] == currentAccount)
-                        ? DimensSizeV2.d12
-                        : 0,
+                bottom: (i < widget.accounts.length - 1 ||
+                        widget.accounts[i] == widget.currentAccount)
+                    ? DimensSizeV2.d12
+                    : 0,
               ),
               child: Row(
                 children: [
                   const SizedBox(width: DimensSizeV2.d24),
                   UserAvatar(
-                    address: accounts[i].address.address,
+                    address: widget.accounts[i].address.address,
                     size: DimensSizeV2.d28,
                     borderRadius: DimensRadiusV2.radius6,
                   ),
@@ -70,42 +84,44 @@ class PublicKeyItemWidget extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        accounts[i].name,
+                        widget.accounts[i].name,
                         style: theme.textStyles.labelMedium,
                       ),
                       const SizedBox(height: DimensSizeV2.d4),
                       Row(
                         children: [
                           Text(
-                            '${accounts[i].address.toEllipseString()} • ',
+                            '${widget.accounts[i].address.toEllipseString()} • ',
                             style: theme.textStyles.labelXSmall.copyWith(
                               color: theme.colors.content3,
                             ),
                           ),
                           StateNotifierBuilder(
-                            listenableState: getBalanceEntity(accounts[i]),
+                            listenableState:
+                                widget.getBalanceEntity(widget.accounts[i]),
                             builder: (_, balance) =>
-                                getBalanceEntity(accounts[i]).let(
-                              (value) => value.value != null
-                                  ? AmountWidget.fromMoney(
-                                      amount: value.value!,
-                                      style:
-                                          theme.textStyles.labelXSmall.copyWith(
-                                        color: theme.colors.content3,
-                                      ),
-                                    )
-                                  : ProgressIndicatorWidget(
-                                      size: DimensSizeV2.d16,
-                                      color: theme.colors.content3,
+                                widget.getBalanceEntity(widget.accounts[i]).let(
+                                      (value) => value.value != null
+                                          ? AmountWidget.fromMoney(
+                                              amount: value.value!,
+                                              style: theme
+                                                  .textStyles.labelXSmall
+                                                  .copyWith(
+                                                color: theme.colors.content3,
+                                              ),
+                                            )
+                                          : ProgressIndicatorWidget(
+                                              size: DimensSizeV2.d16,
+                                              color: theme.colors.content3,
+                                            ),
                                     ),
-                            ),
                           ),
                         ],
                       ),
                     ],
                   ),
                   const Spacer(),
-                  if (accounts[i] == currentAccount)
+                  if (widget.accounts[i] == widget.currentAccount)
                     Padding(
                       padding: const EdgeInsets.only(right: DimensSizeV2.d16),
                       child: Icon(
@@ -123,19 +139,20 @@ class PublicKeyItemWidget extends StatelessWidget {
   }
 
   void _scrollToCurrentAccount(Map<KeyAccount, GlobalKey> itemKeys) {
-    if (currentAccount == null || !itemKeys.containsKey(currentAccount)) {
+    if (widget.currentAccount == null ||
+        !itemKeys.containsKey(widget.currentAccount)) {
       return;
     }
 
-    final currentKey = itemKeys[currentAccount]!;
+    final currentKey = itemKeys[widget.currentAccount]!;
     final context = currentKey.currentContext;
 
-    if (context != null) {
-      final renderBox = context.findRenderObject() as RenderBox;
+    final renderBox = context?.findRenderObject();
+    if (renderBox is RenderBox) {
       final offset = renderBox.localToGlobal(Offset.zero);
-      final scrollOffset = scrollController.offset + offset.dy;
+      final scrollOffset = widget.scrollController.offset + offset.dy;
 
-      scrollController.animateTo(
+      widget.scrollController.animateTo(
         scrollOffset,
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeInOut,
