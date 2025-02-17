94c3618736490188c92859568fcd834d1179cd9a
knightforce <andrianov44@gmail.com>
2025-02-17 22:31:28 +0500

    Remove old import

--- lib/feature/browser/browser.dart
+++ lib/feature/browser/browser.dart
@@ -6,7 +6,6 @@ export 'browser_progress_bar/browser_progress_bar.dart';
 export 'browser_search_bar/browser_search_bar.dart';
 export 'browser_tab_view/browser_tab_view.dart';
 export 'browser_tabs_view/browser_tabs_view.dart';
-export 'history/history.dart';
 export 'inpage_provider/inpage_provider.dart';
 export 'primary/primary.dart';
 export 'tabs/tabs.dart';
